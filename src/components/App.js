c30999689f8172b95ac2e946fb039e1b714e85dc
= <alexander.alegre@outlook.com>
2017-08-06 13:11:21 -0700

    add render reminder list

--- src/components/App.js
+++ src/components/App.js
@@ -15,28 +15,54 @@ class App extends React.Component {
         this.props.addReminder(this.state.text);
     }
 
+    renderReminders() {
+        const { reminders } = this.props;
+        return (
+            <ul className="list-group col-sm-4">
+                {
+                    reminders.map(reminder => {
+                        return(
+                            <li className="list-group-item" key={reminder.id}>
+                                { reminder.text }
+                                <i className="fa fa-remove pull-right block" aria-hidden="true"></i>
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+        );
+    }
+
 
     render() {
         return(
-            <div className="App">
-                <div className="title">
-                    <h1>Hello</h1>
+            <div className="container">
+                <div className="row">
+                    <h1>Reminder Pro</h1>
                 </div>
-                <form action="">
-                    <div className="form-inline">
-                        <div className="form-group">
+                <div className="row">
+                    <form className="form-inline">
+                        <div className="form-group col-sm-12">
                             <input onChange={event => this.setState({text: event.target.value})} type="text" className="form-control" placeholder="I have to ..."/>
                         </div>
-                        <div className="form-group">
+                        <div className="form-group col-sm-12">
                             <button onClick={() => this.addReminder()} type="button" className="btn btn-success">Add Reminder</button>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
+                <div className="row">
+                    { this.renderReminders() }
+                </div>
             </div>
         );
     }
 }
 
+const mapStateToProps = (state) => {
+    return {
+        reminders: state
+    }
+}
 
 
-export default connect(null, {addReminder})(App);
+export default connect(mapStateToProps, {addReminder})(App);
