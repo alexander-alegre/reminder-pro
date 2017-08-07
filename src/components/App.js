99e72ebf755d6f21a21042ce52f97cd5a3ea9688
= <alexander.alegre@outlook.com>
2017-08-06 17:37:26 -0700

    add validation and stying

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { addReminder, deleteReminder, clearReminders } from '../actions/index';
 import moment from 'moment';
+import Alert from 'react-s-alert';
+import 'react-s-alert/dist/s-alert-default.css';
+import 'react-s-alert/dist/s-alert-css-effects/genie.css';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 class App extends React.Component {
     constructor(props) {
@@ -12,8 +16,27 @@ class App extends React.Component {
         }
     }
 
+    showAlert(msg) {
+        Alert.error(msg, {
+            position: 'bottom-right',
+            effect: 'genie',
+            beep: false,
+            timeout: 2000,
+            offset: 0,
+            html: false
+        });
+    }
+
     addReminder() {
-        this.props.addReminder(this.state.text, this.state.dueDate);
+        if(this.state.text && this.state.dueDate) {
+            if (moment(this.state.dueDate) > moment()) {
+                this.props.addReminder(this.state.text, this.state.dueDate);
+            } else {
+                this.showAlert('That item is already past due :( choose a new date.');
+            }
+        } else {
+            this.showAlert('All fields are required!');
+        }
     }
 
     deleteReminder(id) {
@@ -27,15 +50,26 @@ class App extends React.Component {
                 {
                     reminders.map(reminder => {
                         return(
-                            <li className="list-group-item" key={reminder.id}>
-                                <span className="col-6">{reminder.text}</span>
-                                <span className="col-4"><em>
-                                    {
-                                        moment(reminder.dueDate).fromNow()
-                                    }
-                                </em></span>
-                                <i onClick={() => this.deleteReminder(reminder.id)} className="fa fa-remove col-1" aria-hidden="true"></i>
-                            </li>
+                            <ReactCSSTransitionGroup 
+                                key={reminder.id}
+                                transitionName="animate"
+                                transitionAppear={true}
+                                transitionAppearTimeout={500}
+                                transitionEnter={true}
+                                transitionEnterTimeout={500}
+                                transitionLeave={true}
+                                transitionLeaveTimeout={500}
+                            >
+                                <li className="list-group-item">
+                                    <span className="col-6">{reminder.text}</span>
+                                    <span className="col-4"><em>
+                                        {
+                                            moment(reminder.dueDate).fromNow()
+                                        }
+                                    </em></span>
+                                    <i onClick={() => this.deleteReminder(reminder.id)} className="fa fa-remove col-1" aria-hidden="true"></i>
+                                </li>
+                            </ReactCSSTransitionGroup>
                         );
                     })
                 }
@@ -64,9 +98,11 @@ class App extends React.Component {
                 <div className="rendered-list">
                     { this.renderReminders() }
                 </div>
+                <hr/>
                 <button onClick={() => this.props.clearReminders()} className="btn btn-danger">
                     Clear All
                 </button>
+                <Alert stack={true} />
             </div>
         );
     }
